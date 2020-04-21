66487b87379462d194b28229cd1b03acf3e994b3
nicechops <54897878+nicechops@users.noreply.github.com>
2020-04-21 14:14:01 -0400

    Update RoboticsInventory.py

--- RoboticsInventory.py
+++ RoboticsInventory.py
@@ -1,25 +1,25 @@
 import List
 
-class Bin:
+class BaseRecord:
+  def __init__(self, id: int):
+    self.created_at = datetime.datetime.now()
+    self.updated_at = datetime.datetime.now()
+    self.id = id
+    
+class Bin(BaseRecord):
   def __init__(self, id:int, location:str, part_id:str, qty_in_bin:int):
-    self._id = id
+    super().__init__(id)
     self._location = location
     self._part_id = part_id
     self._qty_in_bin = qty_in_bin
-   
-  def get_id(self):
-    return self._id
-  
-  def set_id(self,value:int):
-    assert type(value) is int, "The id must be an integer."
-    self._id = value
-  
+
   def get_location(self):
     return self._location
   
   def set_location(self,value:str):
     assert type(value) is str, "The location must be a string."
     self._location = value
+    self.updated_at = date.datetime.now()
 
   def get_part_id(self):
     return self._part_id
@@ -27,6 +27,7 @@ class Bin:
   def set_part_id(self,value:str):
     assert type(value) is str, "The part id must be a string."
     self._part_id = value
+    self.updated_at = date.datetime.now()
     
   def get_qty_in_bin(self):
     return self._qty_in_bin
@@ -35,20 +36,14 @@ class Bin:
     assert type(value) is int, "The quantity in the bin must be an integer."
     assert value>=0, "The quantity in the bin must be 0 or more".
     self._qty_in_bin = value
+    self.updated_at = date.datetime.now()
     
-class Part:
-  def __init__(self, id:int, name:str, quantity:int, bin_id: int):
-    self._id = id
+class Part(BaseRecord):
+  def __init__(self, part_id:int, name:str, quantity:int, bin_id: int):
+    super().__init__(part_id)
     self._name = name
     self._quantity = quantity
     self._bin_id = bin_id
-    
-  def get_id(self):
-    return self._id
-  
-  def set_id(self,value:int):
-    assert type(value) is int, "The id must be an integer."
-    self._id = value
     
   def get_name(self):
     return self._name
@@ -56,6 +51,7 @@ class Part:
   def set_name(self,value:str):
     assert type(value) is str, "The part name must be a string."
     self._name = value
+    self.updated_at = date.datetime.now()
     
   def get_quantity(self):
     return self._quantity
@@ -64,6 +60,7 @@ class Part:
     assert type(value) is int, "The part quantity must be an integer."
     assert value>=0, "The quantity must be 0 or more."
     self._quantity = value
+    self.updated_at = date.datetime.now()
    
   def get_bin_id(self):
     return self._bin_id
@@ -71,11 +68,13 @@ class Part:
   def set_bin_id(self,value:int):
     assert type(value) is int, "The bin id must be an integer."
     self._bin_id = value
+    self.updated_at = date.datetime.now()
 
-class User:
-  def __init__(self, email:str, student_num:int):
+    
+class User(BaseRecord):
+  def __init__(self,id: int, email:str):
+    super().__init__(id)
     self._email = email
-    self._student_num = student_num
     
   def get_email(self):
     return self._email
@@ -83,41 +82,21 @@ class User:
   def set_email(self, value:str):
     assert type(value) is str, "The user's email must be a string."
     self._email = value
+    self.updated_at = date.datetime.now()
 
-  def get_student_num(self):
-    return self._student_num
-  
-  def set_student_num(self,value:int):
-    assert type(value) is int, "The user's student number must be an integer."
-    self._student_num = value
-    
-class Log:
-  def __init__(self, time:str, user_id:int, part_id:int, quantity:int):
-    self._time = time
-    self._user_id = user_id
+class Log(BaseRecord):
+  def __init__(self, log_id: int, part_id:int, quantity:int):
+    super().__init__(log_id)
     self._part_id = part_id
     self._quantity = quantity
     
-  def get_time(self):
-    return self._time
-  
-  def set_time(self, value:str):
-    assert type(value) is str, "The log time must be a string."
-    self._time = value
-
-  def get_user_id(self):
-    return self._user_id
-  
-  def set_user_id(self,value:int):
-    assert type(value) is int, "The user id must be an integer."
-    self._user_id = value
-
   def get_part_id(self):
     return self._part_id
   
   def set_part_id(self,value:int):
     assert type(value) is int, "The part id must be an integer."
     self._part_id = value
+    self.updated_at = date.datetime.now()
     
   def get_quantity(self):
     return self._quantity
@@ -126,6 +105,7 @@ class Log:
     assert type(value) is int, "The quantity must be an integer."
     assert value>=0, "The quantity must be 0 or more."
     self._quantity = value
+    self.updated_at = date.datetime.now()
     
 class InventoryManager:
   def __init__(self, parts:List[Part], bins:List[Bin], logs:List[Log], users:List[User]):
@@ -133,3 +113,23 @@ class InventoryManager:
     self.bins = bins
     self.logs = logs
     self.users = users
+    
+  def create_user(email: str, student_num: int) -> Student:
+    student_num = User(student_num, email)
+    append.student_num(User)
+    
+  def find_bin_by_location(location: str) -> Bin:
+    for bin in bins:
+      if location == bins[location]
+        return bin
+    
+  def find_user_by_student_num(num: int) -> Student:
+    for student in users:
+      if num == users[num]:
+        return student
+    
+  def add_part(name: str, quantity: int, bin_location: str):
+    name = Part(name, quantity, bin_location)
+    for bin in bins:
+      if name == part_id:
+        find_bin_by_location(name
